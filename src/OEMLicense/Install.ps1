0a75a8cbb974a7b7f8c679849560721f1feb5882
imacks <macksme@outlook.com>
2018-03-03 17:38:39 +0800

    oobe license custom module
    This module allows you to customize the OOBE license agreement page

---
+++ src/OEMLicense/Install.ps1
@@ -0,0 +1,29 @@
+task default -depends Finalize
+
+task Precheck {
+	assert ($BuildEnv.oemLicense.rtfLicense) "The oemLicense.rtfLicense entry is empty or undefined."
+}
+
+task CopyFiles -depends Precheck {
+    $srcFile = Join-Path $BuildEnv.resDir -ChildPath $BuildEnv.oemLicense.rtfLicense
+
+    if (-not (Test-Path $srcFile))
+    {
+        say ("Source file not found: $srcFile") -v 0
+        return
+    }
+
+    $targetFile = Join-Path $BuildEnv.mountDir -ChildPath 'windows\System32\en-US\Licenses\Volume\Enterprise\license.rtf'
+
+	$origAcl = Get-Acl $targetFile
+	Grant-AdminFullFileAccess -Path $targetFile
+
+	say "Replacing target file"
+
+	copy $srcFile $targetFile -Force
+	$origAcl | Set-Acl -Path $targetFile
+}
+
+task Finalize -depends CopyFiles {
+	say 'Done!'
+}
