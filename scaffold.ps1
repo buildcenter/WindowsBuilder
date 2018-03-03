458fb8dac703d1513a38bc1b17f3e99a55542ad9
imacks <macksme@outlook.com>
2018-03-03 18:32:23 +0800

    added scaffold script

---
+++ scaffold.ps1
@@ -0,0 +1,13 @@
+﻿if (Test-Path C:\WindowsBuilder)
+{
+    Write-Error 'A copy of WindowsBuilder is already installed at "C:\WindowsBuilder". Remove the folder and try again.'
+}
+
+if (Test-Path $env:TEMP\windowsbuilder.zip)
+{
+    del $env:TEMP\windowsbuilder.zip
+}
+
+wget 'https://github.com/buildcenter/WindowsBuilder/archive/v1.0.0.zip' -UseBasicParsing -OutFile $env:TEMP\windowsbuilder.zip
+Expand-Archive -Path $env:TEMP\windowsbuilder.zip -DestinationPath C:\
+cd C:\WindowsBuilder
