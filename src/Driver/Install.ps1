15a9d22182e59d4a8b4200711766388cc189e521
imacks <macksme@outlook.com>
2018-03-03 17:39:52 +0800

    driver module bugfix
    Code cleanup

--- src/Driver/Install.ps1
+++ src/Driver/Install.ps1
@@ -3,20 +3,22 @@ task default -depends Finalize
 task Precheck {
 	assert ($BuildEnv.driver) "The 'driver' entry is empty or undefined."
 	assert (($BuildEnv.driver.path -ne '') -and ($BuildEnv.driver.path -is [string])) "The 'driver.path' entry is empty or undefined."
-}}
+}
 
 task AddDrivers -depends Precheck {
 	$mountPath = $BuildEnv.mountDir
+	$driverPath = Join-Path $BuildEnv.resDir -ChildPath $BuildEnv.driver.path
 
-	if (-not (Test-Path $BuildEnv.driver.path -PathType Container))
+	if (-not (Test-Path $driverPath -PathType Container))
 	{
-		say ("The driver resource folder does not exist: {0}" -f $BuildEnv.driver.path)
+		say ("The driver resource folder does not exist: {0}" -f $driverPath)
 		return
 	}
 
 	$addWindowsDriverParams = @{
 		Recurse = $true
-		Path = $BuildEnv.driver.path
+		Path = $mountPath
+        Driver = $driverPath
 	}
 	
 	if ($BuildEnv.driver.unsigned -eq $true)
@@ -24,6 +26,7 @@ task AddDrivers -depends Precheck {
 		$addWindowsDriverParams.ForceUnsigned = $true
 	}
 
+    say ("Adding drivers in {0}" -f $driverPath)
 	Add-WindowsDriver @addWindowsDriverParams
 }
 
